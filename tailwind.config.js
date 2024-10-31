adbeaded1d928328b55bdc25477e5926c46c1ae4
ardszsantos <lokgamer01@gmail.com>
2024-10-30 22:23:24 -0300

    simple tailwind test

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'siteGreen': '#41AEB5'
+      }
+    },
+  },
+  plugins: [],
+}
+
